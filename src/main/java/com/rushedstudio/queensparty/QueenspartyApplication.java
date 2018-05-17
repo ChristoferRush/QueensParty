7a1cd2aa5efd612de09f872cb118d9a196aebb54
Krzysztof Pośpiech <christoferrush@gmail.com>
2018-05-17 14:10:56 +0200

    init commit

---
+++ src/main/java/com/rushedstudio/queensparty/QueenspartyApplication.java
@@ -0,0 +1,12 @@
+package com.rushedstudio.queensparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueenspartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueenspartyApplication.class, args);
+    }
+}
